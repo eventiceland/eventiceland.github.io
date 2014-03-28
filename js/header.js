a54ef25e4cb182f47fd573eb0464cd0ff3d662a1
Brynjar Harðarson <brynjar@gmail.com>
2014-03-28 20:03:47 +0000

    new version

--- js/header.js
+++ js/header.js
@@ -26,12 +26,36 @@ function parallax(){
   var scrolled = $(document).scrollTop();
   if (scrolled < mpos) {
   		//console.log("innnnnn3")
-  		//var bp = Math.round(-(scrolled*0.2));
+  		var bp = Math.round(-(scrolled*0.25));
+  		//console.log(bp)
   		var opacity = parseFloat(1-scrolled*(1/1000)).toFixed(3);
   		//console.log(opacity)
   		//$("#colorfade").css("top", -bp + "px");
-		//$('#header').css('background-position','center ' + );
+		$('#header').css('background-position','center ' + bp + "px" );
 		$('#header').css('opacity', opacity);
   }
   ticking = false;
 }
+
+
+/*$(document).ready(function($) {
+	// init controller
+	var controller = new ScrollMagic();
+	var bp = -248;
+
+	// build tween
+	var tween = TweenMax.fromTo("#header", 0.5, 
+			{"backgroundPosition": "center 0px"},
+			{"backgroundPosition": "center " + bp + "px"}
+		);
+
+	// build scene
+	var elements = $("#message #view");
+	var scene = new ScrollScene({duration: 1200})
+					.addTo(controller)
+					.triggerElement(elements[0])
+					.setTween(tween);
+					
+	// show indicators (requires debug extension)
+	scene.addIndicators();
+});*/
